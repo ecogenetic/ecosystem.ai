434d87a8c998d8fdb493d07a85446c3d7b366233
ecosystem.Ai <jay@ecosystem.ai>
2026-04-26 15:00:45 +0200

    mlrun and security updates

--- content/docs/modules/mlrun/_meta.ts
+++ content/docs/modules/mlrun/_meta.ts
@@ -1,11 +1,12 @@
 export default {
   index: 'Overview',
   install: 'Installation',
+  console_tour: 'Console Tour',
   definition: 'Use-Case Definition',
   data: 'Data Preparation',
   training: 'Model Training',
   deployment: 'Kubernetes Deployment',
-  generator: 'Python Generator',
+  generator: 'Python & AI Generator',
   community_edition: 'MLRun Community Edition',
   access: 'Access & Scoring',
 }
